e8155a2f50b019643808a310848a7f13b3ec4955
Theodore Tzirides <tntath@gmail.com>
2023-07-31 15:47:17 +0300

    chore: add decriptions for the API and routers

--- app/item/router.py
+++ app/item/router.py
@@ -12,40 +12,105 @@ from .schema import Item, ItemCreate
 router = APIRouter()
 
 
-@router.post("/items/", response_model=Item)
+@router.post("/items/", response_model=Item, summary="Create a new item.")
 def create_single_item(item: ItemCreate, db: Session = Depends(get_db)) -> ItemModel:
+    """
+    Create a new item in the system.
+
+    Parameters
+    ----------
+
+    **item** : ItemCreate(title: string, completed: Boolean, deleted: Boolean)
+
+
+    Returns
+    -------
+    Item(id: UUID, title: string, completed: Boolean, deleted: Boolean)
+    """
     return create_item(db, item)
 
 
-@router.get("/items/{item_id}", response_model=Item)
+@router.get("/items/{item_id}", response_model=Item, summary="Get a single item.")
 def get_single_item(item_id: str, db: Session = Depends(get_db)) -> ItemModel:
+    """
+    Get a single item from the system.
+
+    Parameters
+    ----------
+    **item_id**: UUID
+
+    Returns
+    -------
+    Item(id: UUID, title: string, completed: Boolean, deleted: Boolean)
+    """
     item = get_item(db, item_id)
     if item is None:
         raise HTTPException(status_code=404, detail="Item not found")
     return item
 
 
-@router.get("/items/", response_model=List[Item])
+@router.get("/items/", response_model=List[Item], summary="Get all items.")
 def get_all_items(
     db: Session = Depends(get_db),
     completed: Optional[bool] = None,
     deleted: Optional[bool] = None,
 ) -> List[ItemModel]:
+    """
+    Get all items from the system.
+
+    Query parameters
+    ----------
+    **completed**: Boolean
+
+    **deleted**: Boolean
+
+    Returns
+    -------
+    **List[Item]**
+
+    where:
+     Item(id: UUID, title: string, completed: Boolean, deleted: Boolean)
+    """
     return get_items(db, completed=completed, deleted=deleted)
 
 
-@router.put("/items/{item_id}", response_model=Item)
+@router.put("/items/{item_id}", response_model=Item, summary="Update a single item.")
 def update_single_item(
     item_id: str, item: ItemCreate, db: Session = Depends(get_db)
 ) -> ItemModel:
+    """
+    Update a single item in the system.
+
+    Parameters
+    ----------
+    **item_id**: UUID
+
+    **item**: ItemCreate(title: string, completed: Boolean, deleted: Boolean)
+
+    Returns
+    -------
+    Item (id: UUID, title: string, completed: Boolean, deleted: Boolean)
+
+    """
     updated_item = update_item(db, item_id, item)
     if updated_item is None:
         raise HTTPException(status_code=404, detail="Item not found")
     return updated_item
 
 
-@router.delete("/items/{item_id}", response_model=Item)
+@router.delete("/items/{item_id}", response_model=Item, summary="Delete a single item.")
 def remove_single_item(item_id: str, db: Session = Depends(get_db)) -> ItemModel:
+    """
+    Delete a single item from the system.
+
+    Parameters
+    ----------
+    **item_id**: UUID
+
+    Returns
+    -------
+    Item (id: UUID, title: string, completed: Boolean, deleted: Boolean)
+    """
     deleted_item = remove_item(db, item_id)
     if deleted_item is None:
         raise HTTPException(status_code=404, detail="Item not found")
--- main.py
+++ main.py
@@ -2,7 +2,27 @@ from fastapi import FastAPI
 
 from app.item.router import router as item_router
 
-app = FastAPI(title="FastAPI, Docker, and Traefik")
+description = """
+## Description
+
+This is a fully-featured CRUD API for maintaining a Todo list,
+with additional options for listing completed and deleted items.
+
+## Items
+
+You will be able to:
+
+* **Create** a new item
+* **Read** an existing item
+* **Update** an existing item
+* **Delete/Remove** an existing item
+* **List** all items
+* **List** completed items
+* **List** deleted items
+
+"""
+
+app = FastAPI(title="Todo List API", version="0.1.0", description=description)
 
 
 app.include_router(item_router, tags=["items"])
